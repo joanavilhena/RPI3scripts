c6a91fac4632cfd49e73545e7d194d582e4ff851
Joana <2131107@my.ipleiria.pt>
2020-01-04 10:05:58 +0000

    c

--- Python/mqtt_test.py
+++ Python/mqtt_test.py
@@ -1,4 +1,7 @@
 import paho.mqtt.client as paho
+import paho.mqtt.subscribe as subscribe
+
+
 broker="169.254.108.4"
 port=1883
 
@@ -6,12 +9,20 @@ def on_publish(client,userdata,result):             #create function for callbac
     print("data published \n")
     pass
 
+def print_msg(client, userdata, message):
+    print("%s : %s" % (message.topic, message.payload))
+
+
 
 client1= paho.Client("RPI3")
 client1.connect(broker,port)
 
 
+subscribe.callback(print_msg, "#", hostname=broker)
+
 #Subscrever topicos
+
+
 client1.subscribe("test",1)
 
 
