c0264579f8aeffb0e5281877cbe6378c54ba0735
Joana <2131107@my.ipleiria.pt>
2020-01-04 10:38:27 +0000

    c
    c

--- Python/mqtt_publish.py
+++ Python/mqtt_publish.py
@@ -2,8 +2,9 @@ import paho.mqtt.client as paho
 broker="169.254.108.4"
 port=1883
 def on_publish(client,userdata,result):             #create function for callback
-    print("data published \n %s %s %s" % client, userdata, result)
+    print("data published \n")
     pass
+
 client= paho.Client("RPI3")                           #create client object
 client.on_publish = on_publish                          #assign function to callback
 client.connect(broker,port)                                 #establish connection
