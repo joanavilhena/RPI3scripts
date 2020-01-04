50c29ea3509b50d64a4c111f1788b497bbc7838a
Joana <2131107@my.ipleiria.pt>
2020-01-04 22:24:02 +0000

    123

--- Python/mqtt_subscribe.py
+++ Python/mqtt_subscribe.py
@@ -91,39 +91,39 @@ def updateServer(solutionID,espID,value,topic):
     pass
 
 
-  url = "http://206.189.23.62/api/sensorData/update"
+ # url = "http://206.189.23.62/api/sensorData/update"
 
-  payload = "{\r\n\t\"name\" : \"luz\",\r\n\t\"solution_id\" : 2,\r\n\t\"value\": 200,\r\n    \"min_value\": 10,\r\n    \"max_value\": 50\r\n\t\r\n}"
-  headers = {
-    'Content-Type': 'application/json'
-  }
+  #payload = "{\r\n\t\"name\" : \"luz\",\r\n\t\"solution_id\" : 2,\r\n\t\"value\": 200,\r\n    \"min_value\": 10,\r\n    \"max_value\": 50\r\n\t\r\n}"
+  #headers = {
+  #  'Content-Type': 'application/json'
+  #}
 
-  response = requests.request("POST", url, headers=headers, data = payload)
+  #response = requests.request("POST", url, headers=headers, data = payload)
 
-  print(response.text.encode('utf8'))
-  print(response.status_code)
+  #print(response.text.encode('utf8'))
+  #print(response.status_code)
 
 
- # headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+  headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 
- # data = {
- #       "name" : str(topic),
- #       "solution_id" : solutionID,
- #       "value": value,
- #       "min_value": 10,
-  #      "max_value": 50
-	#}
+  data = {
+        "name" : str(topic),
+        "solution_id" : solutionID,
+        "value": value,
+        "min_value": 10,
+        "max_value": 50
+	}
 
-#  r = requests.post(webserver + 'api/sensorData/update', data=data, headers=headers)
- # print(r)
-  #if r.status_code != 200:
-  #  print(r.status_code)
-  #else:
-  #  print(r.status_code)
+  r = requests.post(webserver + 'api/sensorData/update', data=data, headers=headers)
+  print(r)
+  if r.status_code != 201:
+    print(r.status_code)
+  else:
+    print(r.status_code)
     #global token
     #token=r.json()
-   # print("Updated")
-    #pass
+    print("Updated")
+    pass
   pass
 
 def print_msg(client, userdata, message):
