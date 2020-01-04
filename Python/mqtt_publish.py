c1ddc261555651405f09adde19e558f32bdc7d68
Joana <2131107@my.ipleiria.pt>
2020-01-04 10:40:43 +0000

    1

--- Python/mqtt_publish.py
+++ Python/mqtt_publish.py
@@ -5,9 +5,11 @@ def on_publish(client,userdata,result):             #create function for callbac
     print("data published \n")
     pass
 
-client= paho.Client("RPI3")                           #create client object
-client.on_publish = on_publish                          #assign function to callback
-client.connect(broker,port)                                 #establish connection
-ret= client.publish("luz","on")
+while(True):
+    client= paho.Client("RPI3")                           #create client object
+    client.on_publish = on_publish                          #assign function to callback
+    client.connect(broker,port)                                 #establish connection
+    ret= client.publish("luz","on")
 
-client.loop_forever()
+
+
