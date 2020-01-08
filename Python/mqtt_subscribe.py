0a7299c6b637d60ed21812b4c6b2ba4c94d38cb4
Joana <2131107@my.ipleiria.pt>
2020-01-08 22:49:22 +0000

    111

--- Python/mqtt_subscribe.py
+++ Python/mqtt_subscribe.py
@@ -84,7 +84,8 @@ def getServerData():
     r= response.json()
     print(r)
 
-  if r['sensor_data']!="[]"
+   print('{} {}'.format(r['water_force'], r['fan_force']))
+  if r['sensor_data'] != '[]':
 
     for i in r:
       print('{} {}'.format(i['water_force'], i['id']))
