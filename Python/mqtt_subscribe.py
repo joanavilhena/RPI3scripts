4a8a65defdd2533335cbaebfdc226e6bc88f6410
Joana <2131107@my.ipleiria.pt>
2020-01-10 15:34:17 +0000

    123

--- Python/mqtt_subscribe.py
+++ Python/mqtt_subscribe.py
@@ -96,7 +96,7 @@ def createSolution():
     'Content-Type': 'application/json'
   }
 
-  payload = "{\n\t\"toke\": \""+ str(token)+"\",\n\t\"token_hub\": \""str(token_hub)"\"\n\t\n}"
+  payload = "{\n\t\"toke\": \""+ str(token)+"\",\n\t\"token_hub\": \""+str(token_hub)+"\"\n\t\n}"
 headers = {
   'Content-Type': 'application/json'
 }
