64cfb3b0e10854f74977ff7393de450371e9692f
Joana <2131107@my.ipleiria.pt>
2020-01-10 16:46:10 +0000

    213

--- Python/mqtt_subscribe.py
+++ Python/mqtt_subscribe.py
@@ -179,7 +179,7 @@ def createSensorDataWebServer(espID,value,topic):
   url = "http://206.189.23.62/api/sensorData"
 
   #payload = "\r\n{\"name\" : \" %s \", \" solution_id\" :  %d , \"value\": %f  , \"min_value\": 10, \"max_value\": 50}" % (topic, solutionID,value)
-  payload = "\r\n{\"name\" : \""+topic+"\", \"solution_id\" : "+ token +", \"value\": "+str(value)+", \"min_value\": 10, \"max_value\": 50}"
+  payload = "\r\n{\"name\" : \""+topic+"\", \"solution_id\" : "+ token +", \"value\": "+str(value)+", \"min_value\": 30, \"max_value\": 100}"
   #"'{0}' is longer than '{1}'".format(name1, name2)
   #print("%s  %s" % (message.topic, message.payload))
   headers = {'Content-Type': 'application/json'}
